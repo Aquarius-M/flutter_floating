cddf2a5bbe887e359acaabd79410d7b786248408
nephkis <nephkis@163.com>
2024-03-18 09:30:20 +0800

    新增移动方法

---
+++ lib/floating/assist/floating_data.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_floating/floating/assist/Point.dart';
+
+import 'floating_slide_type.dart';
+
+/// @name：floating_data
+/// @package：
+/// @author：345 QQ:1831712732
+/// @time：2022/02/10 17:35
+/// @des：悬浮窗数据记录
+
+class FloatingData {
+  double? left;
+
+  double? top;
+
+  double? right;
+
+  double? bottom;
+
+  double snapToEdgeSpace = 0;
+
+  Point<double>? point;
+
+  FloatingSlideType slideType;
+
+  FloatingData(this.slideType,
+      {this.left, this.top, this.right, this.bottom,this.point, this.snapToEdgeSpace = 0});
+}
---
+++ lib/page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_floating/floating/manager/floating_manager.dart';
+
+/// @name：page
+/// @package：
+/// @author：345 QQ:1831712732
+/// @time：2022/02/16 22:27
+/// @des：
+
+class CustomPage extends StatefulWidget {
+  const CustomPage({Key? key}) : super(key: key);
+
+  @override
+  _CustomPageState createState() => _CustomPageState();
+}
+
+class _CustomPageState extends State<CustomPage> {
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // var s = Get.put(AwesomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("功能页面"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // AwesomeView(),
+            GestureDetector(
+              child: const Text(
+                "关闭悬浮窗",
+                style: TextStyle(fontSize: 30),
+              ),
+              onTap: () {
+                floatingManager.getFloating("1").close();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
+
+// class AwesomeController extends GetxController {
+//   final String title = 'My Awesome View';
+// }
+//
+//
+//
+// // 一定要记住传递你用来注册控制器的`Type`!
+// class AwesomeView extends GetView<AwesomeController> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         floatingManager
+//             .createFloating(
+//                 "1",
+//                 Floating(const FloatingIncrement(),
+//                     slideType: FloatingSlideType.onLeftAndTop,
+//                     left: 0,
+//                     top: 150,
+//                     isShowLog: false,
+//                     slideBottomHeight: 100))
+//             .open(context);
+//       },
+//       child: Container(
+//         padding: EdgeInsets.all(20),
+//         child: Text(controller.title), // 只需调用 "controller.something"。
+//       ),
+//     );
+//   }
+// }
